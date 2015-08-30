78c55ab281e78179a0468f54a96e39b8ed948608
Kyle Gottfried <spitfire1900@live.com>
2015-08-30 18:37:08 -0400

    Add Copyright notice to top of files.

--- BarcodeTestApp/BarcodeTestApp/Properties/AssemblyInfo.cs
+++ BarcodeTestApp/BarcodeTestApp/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("BarcodeTestApp")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Kyle Gottfried")]
 [assembly: AssemblyProduct("BarcodeTestApp")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
